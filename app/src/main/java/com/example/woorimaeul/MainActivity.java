c8fbbbf1b38ce9afb6a245b5f232e5712d76e421
WonJoong <realjoong@gmail.com>
2020-11-01 20:22:06 +0900

    #4 메인 페이지 버튼 초기화

--- app/src/main/java/com/example/woorimaeul/MainActivity.java
+++ app/src/main/java/com/example/woorimaeul/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.woorimaeul;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout button_emergency, button_free, button_ad, button_information;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_emergency = findViewById(R.id.board_emergency);
+        button_free = findViewById(R.id.board_free);
+        button_ad = findViewById(R.id.board_ad);
+        button_information = findViewById(R.id.board_info);
+
+        button_emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), MainActivity.class); // TODO 추후 페이지 만들어지면 수정
+                startActivity(intent);
+            }
+        });
+
+        button_free.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_ad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_information.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //TODO bottom navigation fragments 세팅
     }
 }
